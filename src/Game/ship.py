f5fdafffcccfc3224b073d0a83ddd30fda6e1d7f
Luis Daza <ldaza2394@gmail.com>
2025-03-01 23:43:18 -0400

    The ship class was created and the place boats method was started, it must be finished and the other methods done.

---
+++ src/Game/ship.py
@@ -0,0 +1,14 @@
+class Ship:
+    def __init__(self,name,size,orientation,position):
+        self.name = name
+        self.size = size
+        self.orientation = orientation
+        self.position = position
+        
+
+# def place_boat(self,board):
+#     x,y = self.position
+#     if self.orientation = 'horizontal':
+#         if y + self.size > len(board[])
+#         return False
+    
